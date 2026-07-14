4ed5ae18cd2566df9b0b57bf1160aeb018bb3816
U5 <kyutech.joko@gmail.com>
2026-07-15 01:46:26 +0900

    add: ロゴ

--- src/components/pages/title.tsx
+++ src/components/pages/title.tsx
@@ -1,9 +1,35 @@
+import styled from "styled-components";
 
 export const Title = ({ setGamestate }: { setGamestate: (state: string) => void }) => {
-  return(
-    <div>
-      <h1>タイトル</h1>
-      <button onClick={() => setGamestate("register")}>start</button>
-    </div>
+
+  const TitleWrapper = styled.div`
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+
+  `
+
+  const Button = styled.button`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border: none;
+    cursor: pointer;
+  `
+
+  const Logo = styled.img`
+    position: absolute;
+    width: 100%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  `
+
+  return (
+    <TitleWrapper>
+      <Button onClick={() => setGamestate("register")}>
+        <Logo src="/logo.svg" alt="logo" />
+      </Button>
+    </TitleWrapper>
   );
 };
