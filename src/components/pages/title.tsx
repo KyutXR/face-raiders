f1be71f6a3b42ea5b7a69aaba44bda85d1428b8c
U5 <kyutech.joko@gmail.com>
2026-07-22 01:29:59 +0900

    style: タイトル画面ロゴの表示サイズを拡大
    - logo.svg周囲の透明余白を相殺し、タイトル画面でロゴが大きく表示されるようスタイリングを調整

--- src/components/pages/title.tsx
+++ src/components/pages/title.tsx
@@ -27,9 +27,16 @@ const ContentContainer = styled.div`
   box-sizing: border-box;
 `;
 
+const LogoWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Logo = styled.img`
-  width: 85%;
-  max-width: 380px;
+  width: 140%;
+  max-width: 600px;
   height: auto;
 `;
 
@@ -60,7 +67,9 @@ export const Title = ({ setGamestate }: { setGamestate: (state: string) => void 
   return (
     <TitleWrapper>
       <ContentContainer>
-        <Logo src="/logo.svg" alt="顔 raiders" />
+        <LogoWrapper>
+          <Logo src="/logo.svg" alt="顔 raiders" />
+        </LogoWrapper>
         <StartButton onClick={() => setGamestate("register")}>
           ゲームスタート
         </StartButton>
